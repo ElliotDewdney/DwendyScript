5f252a8603ae7f720ee2e333c74b7d16f9461f5f
NBTX <nbtx@telegenic.me>
2020-01-24 04:25:59 -0600

    Start patching pipe methods for new UI

--- com/samjakob/dwendyscript/ide/RuntimeBridge.java
+++ com/samjakob/dwendyscript/ide/RuntimeBridge.java
@@ -3,15 +3,16 @@ package com.samjakob.dwendyscript.ide;
 import io.github.elliotdewdney.dwendyscript.SimpleReversePolish;
 import javafx.scene.web.WebView;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.*;
 
 public class RuntimeBridge {
 
-    private WebView webView;
+    private static WebView webView;
 
     public RuntimeBridge(WebView webView){
-        this.webView = webView;
+        RuntimeBridge.webView = webView;
     }
 
     public void reloadApplication(){
@@ -25,11 +26,52 @@ public class RuntimeBridge {
 
     public void execute(String program, boolean debug){
         SimpleReversePolish.Pipe pipe = new SimpleReversePolish.Pipe();
+        pipe.DoubleOut = (Double i) -> webView.getEngine().executeScript("Application.addOutput(`" + i + "`)");
+        pipe.IntOut = (int i) -> webView.getEngine().executeScript("Application.addOutput(`" + i + "`)");
+        pipe.StringOut = (String i) -> webView.getEngine().executeScript("Application.addOutput(`" + i + "`)");
+        pipe.Message = (String i) -> webView.getEngine().executeScript("Application.addOutput(`" + i + "`, 'red')");
+        /*pipe.Wait = () -> {
+            //AddToProgramOutput("Program Waiting for user (Submit to continue)", new Color(50, 120, 20));
+            //WaitOnButton();
+        };
+        pipe.StringIn = () -> {
+            AddToProgramOutput("Program Requesting String Input ... ");
+            return WaitOnButton();
+        };
+        pipe.DoubleIn = () -> {
+            AddToProgramOutput("Program Requesting Numerical Input ... ");
+            String temp = WaitOnButton();
+            return temp == null ? 0.0 : Double.parseDouble(temp);
+        };*/
 
         double answer = 0;
         answer = new SimpleReversePolish().AdvancedCalculation(new ArrayDeque<>(Arrays.asList(program.replaceAll("#.+#", "").split("\\s+"))), answer, debug, pipe);
 
-        webView.getEngine().executeScript("Application.terminate(" + answer + ")");
+        RuntimeBridge.$executeScript("Application.terminate(" + answer + ")");
+    }
+
+    public String getSamplePrograms(){
+        List<String> samplePrograms = new ArrayList<>();
+
+        for(SimpleReversePolish.programs program : SimpleReversePolish.programs.values()) {
+            samplePrograms.add("\"" + program.name() + "\"");
+        }
+
+        return Base64.getEncoder().encodeToString(samplePrograms.toString().getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String loadSampleProgram(String name){
+        for(SimpleReversePolish.programs program : SimpleReversePolish.programs.values()) {
+            if(program.name().equals(name)) {
+                return program.program;
+            }
+        }
+
+        return null;
+    }
+
+    private static void $executeScript(String script) {
+        webView.getEngine().executeScript(script);
     }
 
 }
